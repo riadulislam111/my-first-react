4cc3ec87103ee9a00a964b1b38b5e3eb597cf044
Jarif Ahmed <your_github_email@gmail.com>
2026-02-23 19:23:14 +0600

    added all files updates

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -21,3 +21,32 @@
 // export default function ToDo ({task, isDone, time=0}) {
 //     return isDone $$ <li>Done Tasks: {task} time: {times}</li>
 // }
+
+// conditional rendering : 4 $$
+
+// export default function ToDo({task, isDone, time = 0}) {
+//     return isDone $$ <li>Done Tasks: {task} time: {time}</li>
+// }
+
+// condition rendering : 5 ||
+
+// export default function ToDo({task, isDone, time = 0}) {
+//     return isDone || <li>Done Tasks: {task} time: {time}</li>
+// }
+
+
+// conditional rendering: 6 use variable
+
+export default function ToDo({task, isDone}) {
+const displayTime = time ? time : 100;
+
+    let listItem;
+
+    if(isDone === true) {
+        listItem = <li>Done: {task} time: {displayTime}</li>
+    }
+    else {
+        listItem = <li>Pending: {task}</li>
+    }
+    return listItem;
+}
