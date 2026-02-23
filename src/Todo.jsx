eda717927eea10bf0bd0d953312da9194ef0d701
Jarif Ahmed <your_github_email@gmail.com>
2026-02-23 17:07:57 +0600

    added all files updatates

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,11 +4,15 @@
 //     )
 // }
 
-export default function ToDo({task, isDone, time=0}) {
-    if (isDone === true) {
-        return <li>Done: {task} Duration: {time}</li>
-    }
-    else {
-        return <li>Pending: {task}</li>
-    }
+// export default function ToDo({task, isDone, time=0}) {
+//     if (isDone === true) {
+//         return <li>Done: {task} Duration: {time}</li>
+//     }
+//     else {
+//         return <li>Pending: {task}</li>
+//     }
+// }
+
+export default function ToDo({task, isDone, time= 0}) {
+    return isDone ? <li>Done: {task} time: {time}</li>: <li>Not Done: {task}</li>
 }
